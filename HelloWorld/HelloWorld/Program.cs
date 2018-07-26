f4ce9f7d3f7f95989ff857e3a94d061c4eccb544
t1he1maur10ra <jv1@g.toiohomai.ac.nz>
2018-07-26 12:13:05 +1200

    first commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = "Hello World";
+            Console.WriteLine(output);
+
+            // or it can be done on a single line
+            Console.WriteLine("Hello World");
+
+            // We use this console.readline() to stop the application automatically closing.
+            Console.ReadLine();
+        }
+    }
+}
